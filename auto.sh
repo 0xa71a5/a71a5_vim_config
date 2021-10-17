0856e24fc25f5d504575016411268408704f8985
at1a5 <anrist@vip.qq.com>
2021-10-17 16:37:28 +0000

    ENH: optimize cscope find g logic
    Signed-off-by: at1a5 <anrist@vip.qq.com>

--- auto.sh
+++ auto.sh
@@ -1,10 +1,5 @@
 rm -rf ~/.vim*
-touch ~/.vimrc
-rm -rf ~/.vim/auto.sh
-cp vim.tar.gz ~/
-cd ~/
-tar -zxvf vim.tar.gz
-rm ~/vim.tar.gz
 echo "source ~/.vim/vimrcs/main" >> ~/.vimrc
 echo "source ~/.vim/vimrcs/bundle" >> ~/.vimrc
-sudo apt-get install ctags cscope
+cp -R _vim ~/.vim
+echo "done"
